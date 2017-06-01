0256eb39bfef00004c7d956ed52dcf35ef96d289
zubairnahmed <zubair.ahmed@alumni.stanford.edu>
2017-06-01 16:13:35 -0700

    Finish treeNode BST and associated tests

---
+++ src/treeNodeBST.js
@@ -0,0 +1,163 @@
+class TreeNode {
+  constructor(input = null, left = null, right = null) {
+    this.data = input;
+    this.left = left;
+    this.right = right;
+  }
+
+  setData(data) {
+    this.data = data;
+  }
+
+  getData() {
+    return this.data;
+  }
+
+  getLeft() {
+    return this.left;
+  }
+
+  getRight() {
+    return this.right;
+  }
+
+  setLeft(data) {
+    this.left = data;
+  }
+
+  setRight(data) {
+    this.right = data;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = new TreeNode();
+    this.size = 0;
+  }
+
+  insert(data) {
+    if (this.size === 0) {
+      this.root.setData(data);
+      this.size++;
+    }
+    else {
+      const entry = makeEntry(data, this.root);
+      if (entry === 'Duplicates not allowed') return entry;
+    }
+  }
+
+  search(data) {
+    return searchResult(data, this.root);
+  }
+
+  remove(data) {
+    let deleteNode = this.search(data);
+
+    if(deleteNode.left === null && deleteNode.right === null) {
+      processNoChildDeletion(data, this.root);
+    }
+    else if (deleteNode.left === null || deleteNode.right === null) {
+      if (deleteNode.left !== null) {
+        deleteNode.data = deleteNode.left.data;
+        deleteNode.left = null;
+      }
+      else {
+        deleteNode.data = deleteNode.right.data;
+        deleteNode.right = null;
+      }
+    }
+    else {
+      processTwoChildDeletion(deleteNode);
+    }
+  }
+
+  traverse(callback) {
+    executeTraversal(this.root, callback);
+  }
+
+  count() {
+    return this.size;
+  }
+
+  getRoot() {
+    return this.root;
+  }
+}
+
+function makeEntry(data, node) {
+  if (data === node.data) {
+    return 'Duplicates not allowed';
+  }
+  else if (node.left === null && data < node.data) {
+    const entry = new TreeNode(data);
+    node.left = entry;
+  }
+  else if (node.right === null && data > node.data) {
+    const entry = new TreeNode(data);
+    node.right = entry;
+  }
+  else if (data < node.data) {
+    makeEntry(data, node.left);
+  }
+  else {
+    makeEntry(data, node.right);
+  }
+}
+
+function searchResult(data, node) {
+  if (node === null) return null;
+  if (data === node.data) return node;
+  else if (data < node.data) return searchResult(data, node.left);
+  else return searchResult(data, node.right);
+}
+
+function processNoChildDeletion(data, node) {
+  let currentNode = node;
+  let previousNode;
+
+  while (data !== currentNode.data) {
+    if (data < currentNode.data) {
+      previousNode = currentNode;
+      currentNode = currentNode.left;
+    }
+    else {
+      previousNode = currentNode;
+      currentNode = currentNode.right;
+    }
+  }
+
+  if (data === previousNode.left.data) previousNode.left = null;
+  else previousNode.right = null;
+}
+
+function processTwoChildDeletion(node) {
+  const goRight = node.right;
+  let goLeft = goRight.left;
+  let inOrderSuccessor;
+
+  while (goLeft !== null) {
+    inOrderSuccessor = goLeft;
+    goLeft = goLeft.left;
+  }
+
+  node.data = inOrderSuccessor.data;
+
+  if (inOrderSuccessor.left !== null) {
+    inOrderSuccessor.data = inOrderSuccessor.left.data;
+    inOrderSuccessor.left = null;
+  }
+  else if (inOrderSuccessor.right !== null) {
+    inOrderSuccessor.data = inOrderSuccessor.right.data;
+    inOrderSuccessor.right = null;
+  }
+}
+
+function executeTraversal(node, callback) {
+  if (node === null) return;
+  executeTraversal(node.left, callback);
+  callback(node.data);
+  executeTraversal(node.right, callback);
+}
+
+export { TreeNode, BinarySearchTree };
